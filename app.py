ae460ca4ff957e18f79dc7b8ab70f528f8dc226b
Sasch <sascha.zierke@outlook.com>
2021-10-14 15:00:10 +0200

    fix incorrect return type

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import flask
+from bson import json_util
 from flask import Flask
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
+CORS(app)
 mongo = PyMongo(app)
 
 
@@ -12,27 +17,34 @@ def home():
     return
 
 
-@app.route('/residential')
+@app.route('/residential', methods=['GET'])
 def find_residential():  # put application's code here
     test = mongo.db.acs.find_one({"name": "Residential"})
-    print(test)
-    return dumps(test)
+    response = flask.jsonify(parse_json(test))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
-@app.route('/commercial')
+@app.route('/commercial', methods=['GET'])
 def find_commercial():  # put application's code here
     test = mongo.db.acs.find_one({"name": "Commercial"})
-    print(test)
-    return dumps(test)
+    response = flask.jsonify(parse_json(test))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
-@app.route('/healthcare')
+@app.route('/healthcare', methods=['GET'])
 def find_health_care():  # put application's code here
     test = mongo.db.acs.find_one({"name": "Health Care"})
-    print(test)
-    return dumps(test)
+    response = flask.jsonify(parse_json(test))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
